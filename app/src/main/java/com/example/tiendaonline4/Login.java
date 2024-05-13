6a5e8869a0661e8ee5aeb980cb9a64ec617d225b
DavidreySP <159583182+DavidreySP@users.noreply.github.com>
2024-05-13 18:15:09 -0500

    act

--- app/src/main/java/com/example/tiendaonline4/Login.java
+++ app/src/main/java/com/example/tiendaonline4/Login.java
@@ -3,8 +3,10 @@ package com.example.tiendaonline4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,15 +14,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private TextInputEditText loginEmail, loginPassword;
+    private Button loginButton;
     DatabaseReference reference;
 
     @Override
@@ -28,55 +37,39 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        FirebaseApp.initializeApp(getApplicationContext());
-    }
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.email_loginET);
+        loginPassword = findViewById(R.id.clave_loginET);
+        loginButton = findViewById(R.id.login_check);
 
-    public void login_check(View view){
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
 
-        TextInputEditText email = findViewById(R.id.email_loginET);
-        TextInputEditText password = findViewById(R.id.clave_loginET);
-
-        String email_text = email.getText().toString();
-        String password_text = password.getText().toString();
-
-        Intent i = new Intent(getApplicationContext(), RegistrosClientes.class);
-        startActivity(i);
-        finish();
-
-        if(email_text.isEmpty() || password_text.isEmpty()){
-            Toast.makeText(getApplicationContext(), "Por favor ingresar correo y contraseña", Toast.LENGTH_SHORT).show();
-        }else{
-            reference = FirebaseDatabase.getInstance().getReference("clientes");
-            reference.child(email_text).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DataSnapshot> task) {
-                    Log.d("Usuario Ingresado", "el usuario ingresado fue: "+email_text);
-                    if(task.isSuccessful()){
-                        if(task.getResult().exists()){
-                            DataSnapshot dataSnapshot = task.getResult();
-                            String password_aux = String.valueOf(dataSnapshot.child("clave").getValue());
-
-                            if(password_aux.equals(password_text)){
-                                Toast.makeText(getApplicationContext(), "Inicio de sesion exitoso", Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(getApplicationContext(), RegistrosClientes.class);
-                                startActivity(i);
-                                finish();
-                            }else{
-                                Toast.makeText(getApplicationContext(), "Email o clave incorrectos", Toast.LENGTH_SHORT).show();
-                            }
-
-                        }else{
-                            Toast.makeText(getApplicationContext(), "Usuario ingresado no existe", Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-                        Toast.makeText(getApplicationContext(), "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if (!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(Login.this, "Ingreso Exitoso", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(Login.this, Lista_Productos.class));
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this, "Ingreso Fallido", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        loginPassword.setError("No pueden haber campos vacios");
                     }
+                }else if(email.isEmpty()){
+                    loginEmail.setError("No pueden haber campos vacios");
                 }
-            });
-
-
-        }
+            }
+        });
     }
-
-
 }
